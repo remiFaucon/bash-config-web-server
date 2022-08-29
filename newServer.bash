dac88e599b03b94fd5db1788d6dac49340c103c2
faucon <faucon.remi04@gmail.com>
2022-08-29 03:10:33 +0200

    nodejs site + websocket

--- newServer.bash
+++ newServer.bash
@@ -13,9 +13,10 @@ while test $# -gt 0; do
       echo "options:"
       echo "-h, --help                show brief help"
       echo "-n, --name                name of the web site"
+      echo "-s, --secure              setup ssl"
       echo "-t, --type=TYPE           static|php|nodejs"
       echo "-p, --port=PORT           port of your app nodejs"
-      echo "-s, --secure              setup ssl"
+      echo "-ws, --web-socket         active web socket"
       exit 0
       ;;
 
@@ -58,6 +59,11 @@ while test $# -gt 0; do
       shift
       ;;
 
+    -ws|--web-socket)
+      webSocket=true
+      shift
+      ;;
+
     -s|--secure)
       secure=true
       shift
@@ -70,14 +76,14 @@ if test -z "$type" -a -z "$name"; then
   exit 1
 else
   if test "$type" = "static"; then
-      echo "server {
+    echo "server {
 
   listen 80;
   server_name $name;
 
   # Les urls commençant par / (toutes les urls)
   location / {
-      root /home/dev/www;
+      root /home/prod/$name/www;
       index index.html index.htm;
       try_files \$uri \$uri/ \$uri.html =404;
   }
@@ -90,8 +96,8 @@ else
   }
 
   # On va placer les logs dans un dossier accessible
-  error_log /home/dev/logs/error.log;
-  access_log /home/dev/logs/acess.log;
+  error_log /home/prod/$name/logs/error.log;
+  access_log /home/prod/$name/logs/acess.log;
 
   # Les pages d'erreurs
   error_page 404 500 501 /error.html;
@@ -105,11 +111,72 @@ server {
   return 301 http://$name\$request_uri;
 }" > "$rootFolder"/"$name".conf
 
+    cmd "$(sudo mkdir /home/prod/"$name"/www)"
+    cmd "$(sudo mkdir /home/prod/"$name"/logs)"
+
+    cmd "$(sudo nginx -t)"
+    cmd "$(sudo service nginx restart)"
+    exit 1
+
+  elif test "$type" = "nodejs"; then
+    if test -z "$port"; then
+      echo "port is not define"
+      exit 1
+
+    elif test -z "$webSocket"; then
+      echo "upstream $name {
+  server localhost:$port;
+}
+
+server {
+
+  listen 80;
+  listen [::]:80;
+  server_name $name;
+
+  location / {
+      proxy_pass http://$name;
+  }
+}" > "$rootFolder"/"$name".conf
+
+      cmd "$(pm2 start /home/prod/"$name"/serverjs)"
       cmd "$(sudo nginx -t)"
       cmd "$(sudo service nginx restart)"
       exit 1
-    else test "$type" = "nodejs" -a -z "$port"
-      echo "port is not define"
+
+    else
+      echo "upstream $name {
+    server localhost:$port;
+}
+
+server {
+
+    listen 80;
+    listen [::]:80;
+    server_name $name;
+
+    location / {
+        proxy_pass http://$name;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \"upgrade\";
+    }
+}" > "$rootFolder"/"$name".conf
+
+      cmd "$(sudo mkdir /home/prod/"$name")"
+      cmd "$(pm2 start /home/prod/"$name"/server.js)"
+      cmd "$(sudo nginx -t)"
+      cmd "$(sudo service nginx restart)"
       exit 1
     fi
+
+    elif test "$type" = "php"; then
+      echo "" > "$rootFolder"/"$name".conf
+
+      md "$(sudo mkdir /home/prod/"$name")"
+      cmd "$(pm2 start /home/prod/"$name"/server.js)"
+      cmd "$(sudo nginx -t)"
+      cmd "$(sudo service nginx restart)"
+      exit 1
+  fi
 fi
