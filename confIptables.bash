b991212d095c4b42cae27896ed74010df144cb6e
faucon <faucon.remi04@gmail.com>
2022-09-10 13:11:53 +0200

    iptables fix 2

--- confIptables.bash
+++ confIptables.bash
@@ -7,8 +7,8 @@ iptables -t filter -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -t filter -A INPUT -p tcp -dport 22 -j ACCEPT
 iptables -t filter -A INPUT -i lo -j ACCEPT
 iptables -t filter -A OUTPUT -i lo -j ACCEPT
-iptables -t filter -A INPUT -p tcp -dport 80 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp -dport 80 -j ACCEPT
-iptables -t filter -A INPUT -p tcp -dport 443 -j ACCEPT
-iptables -t filter -A OUTPUT -p tcp -dport 443 -j ACCEPT
-iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
+iptables -t filter -A INPUT -p TCP --dport 80 -j ACCEPT
+iptables -t filter -A OUTPUT -p TCP --dport 80 -j ACCEPT
+iptables -t filter -A INPUT -p TCP --dport 443 -j ACCEPT
+iptables -t filter -A OUTPUT -p TCP --dport 443 -j ACCEPT
+iptables -t filter -A OUTPUT -p UDP --dport 123 -j ACCEPT
