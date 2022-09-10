365236b4e3fc5d3227a45bb23de1277f600d2111
faucon <faucon.remi04@gmail.com>
2022-09-10 13:02:33 +0200

    iptables fix

--- confIptables.bash
+++ confIptables.bash
@@ -2,13 +2,13 @@ iptables -F
 iptables -P OUTPUT DROP
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -p tcp -dport 22 -j ACCEPT
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -i lo -j ACCEPT
-iptables -A INPUT -p tcp -dport 80 -j ACCEPT
-iptables -A OUTPUT -p tcp -dport 80 -j ACCEPT
-iptables -A INPUT -p tcp -dport 443 -j ACCEPT
-iptables -A OUTPUT -p tcp -dport 443 -j ACCEPT
+iptables -t filter -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -t filter -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -t filter -A INPUT -p tcp -dport 22 -j ACCEPT
+iptables -t filter -A INPUT -i lo -j ACCEPT
+iptables -t filter -A OUTPUT -i lo -j ACCEPT
+iptables -t filter -A INPUT -p tcp -dport 80 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp -dport 80 -j ACCEPT
+iptables -t filter -A INPUT -p tcp -dport 443 -j ACCEPT
+iptables -t filter -A OUTPUT -p tcp -dport 443 -j ACCEPT
 iptables -t filter -A OUTPUT -p udp --dport 123 -j ACCEPT
