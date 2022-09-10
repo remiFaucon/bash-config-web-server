1ea9f084a088a8f4f0fe2c09d9936bc5d705d002
faucon <faucon.remi04@gmail.com>
2022-09-10 13:22:51 +0200

    iptables fix 3

--- confIptables.bash
+++ confIptables.bash
@@ -5,10 +5,10 @@ iptables -P FORWARD DROP
 iptables -t filter -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -t filter -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -t filter -A INPUT -p tcp -dport 22 -j ACCEPT
-iptables -t filter -A INPUT -i lo -j ACCEPT
-iptables -t filter -A OUTPUT -i lo -j ACCEPT
-iptables -t filter -A INPUT -p TCP --dport 80 -j ACCEPT
-iptables -t filter -A OUTPUT -p TCP --dport 80 -j ACCEPT
-iptables -t filter -A INPUT -p TCP --dport 443 -j ACCEPT
-iptables -t filter -A OUTPUT -p TCP --dport 443 -j ACCEPT
-iptables -t filter -A OUTPUT -p UDP --dport 123 -j ACCEPT
+#iptables -t filter -A INPUT -i lo -j ACCEPT
+#iptables -t filter -A OUTPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 80 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -t filter -A OUTPUT -p UDP --sport 123 -j ACCEPT
